66facfb0a041251537ba5525f2bbd6be673ee1cf
bcm <bcmardini@gmail.com>
2025-11-20 20:38:23 -0300

    val loss fix

--- RNN/rnn_train.py
+++ RNN/rnn_train.py
@@ -98,7 +98,7 @@ if __name__ == '__main__':
                 l_done = F.binary_cross_entropy_with_logits(pred_d_logits.squeeze(-1), d_next)
 
                 # val_loss += (l_mdn + l_reward + l_done).item()
-                val_loss = l_mdn + (20.0 * l_reward) + (20.0 * l_done)
+                val_loss += l_mdn + (20.0 * l_reward) + (20.0 * l_done)
                 # val_loss += l_mdn.item()
 
         train_loss /= len(train_loader)
